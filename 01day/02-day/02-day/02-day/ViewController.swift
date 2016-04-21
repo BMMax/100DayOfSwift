9db80aee3b5019098fa6623127613241eb7281f1
mobin <mobin-888@163.com>
2016-04-21 10:24:42 +0800

    02-day-ad
    02-day-ad

---
+++ 01day/02-day/02-day/02-day/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  02-day
+//
+//  Created by user on 16/4/21.
+//  Copyright © 2016年 mobin. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var adView: ADBannerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController:ADBannerViewDelegate{
+
+
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        self.adView.hidden = false
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        print("fail")
+    }
+
+
+}
+
+
